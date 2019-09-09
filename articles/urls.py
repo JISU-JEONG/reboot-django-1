ca37c07f9d614e40c3384d2ad6c85bdb916f0e1f
EHwooKim <ehwoo0707@naver.com>
2019-09-09 15:36:03 +0900

    190909

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,11 @@ from . import views
 # path, views도 중요하지만. urlpatterns 라는 변수가 필수적이다아!!!
 urlpatterns = [
     path('', views.index),
+    path('new/', views.new),
+    path('create/', views.create),
+    path('<int:pk>/', views.detail),
+    path('<int:pk>/edit/', views.edit),
+    path('<int:pk>/update/', views.update),
+    path('<int:pk>/delete/', views.delete),
 
 ]
